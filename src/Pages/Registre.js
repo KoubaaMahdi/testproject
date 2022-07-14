15129b94385bea8a84ccf4ae9e660850306ae871
MahdiKoubaa <Mahdi.koubaa@outlook.com>
2022-07-14 21:42:53 +0100

    Correction CreateAccount

--- src/Pages/Registre.js
+++ src/Pages/Registre.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import '../styles.css';
-import RegistreForm from "../components/RegistreForm ";
+import RegistreForm from "../components/RegistreForm";
 const Registre = props => (
   <RegistreForm />
 );
--- src/components/Form2.jsx
+++ src/components/Form2.jsx
@@ -3,13 +3,12 @@ import { ReactDOM } from "react";
 import FormButton from "./FormButton";
 import FormInput from "./FormInput";
 import FormCreateAccount from "./FormCreateAccount"
-const Form = props => (
+const Form2 = props => (
     <div>
       <FormInput description="Username" placeholder="Enter your username" type="text" />
       <FormInput description="Password" placeholder="Enter your password" type="password"/>
       <FormInput description="email" placeholder="Enter your email" type="email"/>
       <FormButton title="Log in"/>
-      <FormCreateAccount/>
     </div>
    );
 
