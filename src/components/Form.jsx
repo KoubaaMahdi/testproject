466b302a43c01018331f439e514edacdc489963d
MahdiKoubaa <Mahdi.koubaa@outlook.com>
2022-07-14 20:27:23 +0100

    first day 7/14/2022

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { ReactDOM } from "react";
 import FormButton from "./FormButton";
 import FormInput from "./FormInput";
+import FormCreateAccount from "./FormCreateAccount"
 const Form = props => (
     <div>
       <FormInput description="Username" placeholder="Enter your username" type="text" />
       <FormInput description="Password" placeholder="Enter your password" type="password"/>
       <FormButton title="Log in"/>
+      <FormCreateAccount/>
     </div>
    );
 
-   export default Form
+   export default Form;
---
+++ src/components/FormCreateAccount.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const FormCreateAccount = props => (
+    <div>
+        <p><a href="detection.html">Create Account</a></p>
+    </div>
+);
+export default FormCreateAccount ;
